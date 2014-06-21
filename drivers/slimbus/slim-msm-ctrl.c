036ecb4ebca4356d3c5f0195fae4169dfbe40323
flar2 <asegaert@gmail.com>
2014-06-21 00:19:34 -0400

    Prevent touches during phone calls
    Signed-off-by: flar2 <asegaert@gmail.com>

--- drivers/slimbus/slim-msm-ctrl.c
+++ drivers/slimbus/slim-msm-ctrl.c
@@ -26,6 +26,11 @@
 #include <linux/of_slimbus.h>
 #include <mach/sps.h>
 
+#ifdef CONFIG_TOUCHSCREEN_SYNAPTICS_SWEEP2WAKE
+#include <linux/synaptics_i2c_rmi.h>
+int in_phone_call = 0;
+#endif
+
 #define SLIM_RX_MSGQ_BUF_LEN	40
 
 #define SLIM_USR_MC_GENERIC_ACK		0x25
@@ -1392,6 +1397,9 @@ send_capability:
 			gen_ack = true;
 			pr_info("[AUD] SAT connect MC:0x%x,LA:0x%x", txn.mc,
 					sat->satcl.laddr);
+#ifdef CONFIG_TOUCHSCREEN_SYNAPTICS_SWEEP2WAKE
+			in_phone_call = 1;
+#endif
 			ret = msm_xfer_msg(&dev->ctrl, &txn);
 			break;
 		case SLIM_USR_MC_DISCONNECT_PORT:
@@ -1405,6 +1413,9 @@ send_capability:
 			txn.wbuf = wbuf;
 			gen_ack = true;
 			pr_info("[AUD] SAT disconnect LA:0x%x", sat->satcl.laddr);
+#ifdef CONFIG_TOUCHSCREEN_SYNAPTICS_SWEEP2WAKE
+			in_phone_call = 0;
+#endif
 			ret = msm_xfer_msg(&dev->ctrl, &txn);
 		default:
 			break;
